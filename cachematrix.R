d1ac74cc158ed5b116025fb3343336de2e9df544
erichart07 <eric.hart.07@gmail.com>
2015-05-22 11:38:59 -0400

    Second attempt (hopefully last)

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,39 @@
-## Put comments here that give an overall description of what your
-## functions do
+## These two functions allow the ability to compute and cache the inverse of a matrix
 
-## Write a short comment describing this function
+## makeCacheMatrix defines a matrix object. It has matrix and inverse attributes,
+## with getters and setters for each
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        inv <- NULL                     # Instantiate matrix objects without inverse
+        set <- function(y) {            # Setter function for matrix attribute
+                x <<- y
+                inv <<- NULL            # Setting the matrix resets the inverse (to null)
+        }
+        get <- function() x             # Getter function for matrix attribute
+        setinv <- function(usrinv) inv <<- usrinv       # Setter function for inverse attribute
+        getinv <- function() inv                        # Getter function for inverse attribut
+        list(set = set, get = get,      # List the functions when an object is instantiated
+             setinv = setinv,
+             getinv = getinv)
 }
 
 
-## Write a short comment describing this function
+## cacheSolve returns the inverse of a matrix either from memory if it is cached, 
+## or by computing it if necessary
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        inv <- x$getinv()               # Get the inverse attribute from the matrix object using the getter
+        if(!is.null(inv)) {                     # If the inverse attribute exists, indicate that the inverse
+                message("getting cached data")  # came from the cache, prepare to return it
+                returnme <- inv
+        }
+        else{                           # If the inverse attribute was null, get the matrix, compute the inverse, 
+        matrix <- x$get()               # set the inverse attribute, and prepare to return the inverse
+        inv <- solve(matrix)
+        x$setinv(inv)
+        returnme <- inv
+        }
+        
+        returnme                                       ## Return the inverse 'x'
 }
+
